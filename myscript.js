cae15f2562e8e6a555d50d29f97b97da820ddf0d
Slava <vjacheslav_leus@mail.ru>
2017-10-26 08:42:31 -0400

    Removed boards

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+var xLen = 800;
+var yLen = 600;
 var step = 20;
 var rect = {x:0,y:0,dx:step,dy:step}
 var gKeyCode = 39;
@@ -42,6 +44,16 @@ function move()
 				rect.y += step;
 				break;
 	}
+
+	if (rect.x < 0)
+		rect.x = xLen - 1;
+	else if (rect.x >= xLen)
+		rect.x = 0;
+	
+	if (rect.y < 0)
+		rect.y = yLen - 1;
+	else if (rect.y >= yLen)
+		rect.y = 0;
 	
 	paintRect();
 }
@@ -52,24 +64,5 @@ function KeyDown(event)
 	var ctx = canvas.getContext("2d");
 	ctx.clearRect(rect.x-1, rect.y-1, rect.dx+2, rect.dy+2);
 	gKeyCode = event.keyCode;
-	switch (gKeyCode)
-	{
-			case 37: // left
-				rect.x -= step;
-				break;
-				
-			case 38: // up
-				rect.y -= step;
-				break;
-				
-			case 39: // right
-				rect.x += step;
-				break;
-				
-			case 40: // down
-				rect.y += step;
-				break;
-	}
-	
-	paintRect();
+	move();
 }
